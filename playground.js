b76b8c1a53d88142e2b28aaa572be47e928cda3e
Max <nanayawfixing@gmail.com>
2024-02-06 18:51:35 +0000

    sss

---
+++ playground.js
@@ -0,0 +1,8 @@
+console.log("hi");
+// Path: playground.js
+
+
+const myArr = [1,2,3,4];
+// myArr.concat([3,2,1], [3,2,1])
+// console.log(myArr.splice(3));// splice changes the myArr and doesnt create a copy
+// console.log(myArr);
