e59a89c07f5152fd28c601d07dc22ffda0917d5e
Max <nanayawfixing@gmail.com>
2024-05-16 17:17:37 +0000

    good day

--- playground.js
+++ playground.js
@@ -1,48 +1,3 @@
-const fantanResult = {
-  1: [
-    "Big",
-    "Small",
-    "Odd",
-    "Even",
-    "Ones",
-    "Tens",
-    "Hundreds",
-    "Thousands",
-    "Ten Thousands",
-    "Dragon",
-    "Tiger",
-    "Tie",
-    "1,2",
-    "2,3",
-    "1,3",
-    "1,4",
-    "2,4",
-    "3,4",
-    "2,3,4",
-    "3,4,1",
-    "2,4,1",
-    "3,2,1",
-    "1",
-    "2",
-    "3",
-    "4",
-  ],
-  2: [],
-  3: [],
-  4: [],
-  5: [],
-};
-
-// function fantan5d(drawNumbers){}
-
-// class FantanGame {
-//   constructor(lotteryType, drawNumbers) {
-//     this.lotteryType = lotteryType;
-//     this.drawNumbers = drawNumbers;
-//   }
-
-// }
-
 function bigSmall(num, bigMin, bigMax, smallMin, smallMax) {
   if (num >= bigMin && num <= bigMax) {
     return "Big";
@@ -53,19 +8,6 @@ function bigSmall(num, bigMin, bigMax, smallMin, smallMax) {
   }
 }
 
-console.log(
-  bigSmall(
-    [9, 8, 1, 2, 0].slice(2).reduce((acc, curr, array) => acc + curr, 0),
-    23,
-    45,
-    0,
-    22
-  )
-);
-console.log(
-  oddEven([9, 8, 1, 2, 0].reduce((acc, curr, array) => acc + curr, 0))
-);
-
 function oddEven(num) {
   if (num % 2 === 0) {
     return "Even";
@@ -73,8 +15,6 @@ function oddEven(num) {
     return "Odd";
   }
 }
-
-const tripleComb = ["2,3,4", "1,3,4", "1,2,4", "1,2,3"];
 
 function fantan1(num) {
   let remainder = num % 4;
@@ -138,8 +78,6 @@ function fantan2(num) {
   }
 }
 
-// function fantan3() {}
-
 function getColorRange(num) {
   if (num >= 0 && num <= 3) {
     return "Red";
@@ -160,13 +98,34 @@ function getColorRange(num) {
   }
 }
 
+function naturalElements(number) {
+  if (number >= 3 && number <= 7) {
+    return "Wind";
+  } else if (number >= 8 && number <= 10) {
+    return "Fire";
+  } else if (number >= 11 && number <= 13) {
+    return "Thunder";
+  } else if (number >= 14 && number <= 18) {
+    return "Electricity";
+  } else {
+    return "Number out of range";
+  }
+}
+
+function isPair(num1, num2, num3) {
+  return num1 === num2 && num2 === num3;
+}
+
+function isTriple(num1, num2, num3) {
+  return num1 === num2 && num2 === num3;
+}
+
 function getWinCombinations(num1, num2, num3) {
   const isBig = (num) => num >= 5 && num <= 9;
   const isSmall = (num) => num >= 0 && num <= 4;
   const isEven = (num) => num % 2 === 0;
   const isOdd = (num) => num !== 0 && num % 2 !== 0; // Corrected isOdd function
 
-  const isPair = (num1, num2, num3) => num1 === num2 && num2 === num3;
   const isAllBig = (num1, num2, num3) =>
     isBig(num1) && isBig(num2) && isBig(num3);
   const isAllSmall = (num1, num2, num3) =>
@@ -183,7 +142,7 @@ function getWinCombinations(num1, num2, num3) {
 
   const winCombinations = [];
 
-  if (isPair(num1, num2, num3)) {
+  if (sPair([num1, num2, num3])[0]) {
     winCombinations.push("Pair");
   }
   if (moreBig) {
@@ -210,16 +169,12 @@ function getWinCombinations(num1, num2, num3) {
   if (isAllOdd(num1, num2, num3)) {
     winCombinations.push("All Odd");
   }
-  if (!isPair(num1, num2, num3)) {
+  if (!sPair([num1, num2, num3])[0]) {
     winCombinations.push("No Pair");
   }
 
   return winCombinations;
 }
-
-// console.log(getWinCombinations(...[9,2,1,2,5].slice(2)));
-
-// console.log([1, 0, 9, 3, 0].slice(2));
 
 function sumAndRange(arr) {
   // Helper function to check if a string contains only digits
@@ -242,8 +197,6 @@ function sumAndRange(arr) {
     return `${sum}`; //`Sum is ${sum}`;
   }
 }
-
-// console.log(sumAndRange([8, 8, 8]));
 
 function positionOfBallNode(nums) {
   const isBig = (num) => num >= 5 && num <= 9;
@@ -282,26 +235,9 @@ function positionOfBallNode(nums) {
   return result;
 }
 
-// console.log(positionOfBallNode([4, 2, 2, 1, 7]));
-
-// console.log(
-//   getColorRange(
-//     [9,2,1,2,5].slice(2).reduce((acc, curr, array) => acc + curr, 0)
-//   )
-// );
-// console.log("fantan 2", fantan1([9,1,5,0,1].slice(2).reduce((acc, curr, array) => acc + curr, 0)))
-
-// const a = [1, 2, 3, 4];
-
-// console.log("min", Math.max(...a), a[0]);
-
-// let g = a.map((item, ind, arr) => [Math.max(...arr), ind]);
-
-// console.log([1, 2, 3, 4].findIndex(() => Math.max(...[1, 2, 3, 4])));
-
 function findHighestPosition(arr) {
   const maxValue = Math.max(...arr);
-  const position = ["Ten Thousand", "Thousand", "Hundred", "Ten", "Zero"]
+  const position = ["Ten Thousand", "Thousand", "Hundred", "Ten", "Zero"];
   const maxIndexes = arr.reduce((indexes, value, index) => {
     if (value === maxValue) {
       indexes.push(position[index]);
@@ -311,11 +247,219 @@ function findHighestPosition(arr) {
   return maxIndexes;
 }
 
-console.log(findHighestPosition([9, 2, 3, 4, 9]));
-
-
-
-
-function fiveDFantanResults(drawNumber){
-
+function getDragonTiger(drawNumbers) {
+  if (drawNumbers[0] > drawNumbers[4]) {
+    return "Dragon";
+  } else if (drawNumbers[0] < drawNumbers[4]) {
+    return "Tiger";
+  } else {
+    return "Tie";
+  }
 }
+
+function fiveDFantanResults(drawNumber, game, optionalBallPosition = 1) {
+  const DRAW_NUMBERS = [...drawNumber];
+  const GAME_RESULTS = [];
+  const SUM_OF_THREE = DRAW_NUMBERS.slice(2).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+
+  switch (game.toLowerCase()) {
+    case "main area": {
+      GAME_RESULTS.push(
+        bigSmall(DRAW_NUMBERS, 23, 45, 0, 22),
+        oddEven(DRAW_NUMBERS),
+        getDragonTiger(DRAW_NUMBERS),
+        ...findHighestPosition(DRAW_NUMBERS)
+      );
+      break;
+    }
+    case "fantan 1": {
+      GAME_RESULTS.push(...fantan1(SUM_OF_THREE));
+      break;
+    }
+    case "fantan 2": {
+      GAME_RESULTS.push(...fantan2(SUM_OF_THREE));
+      break;
+    }
+    case "sum of last 3": {
+      GAME_RESULTS.push(
+        // getColorRange(SUM_OF_THREE),
+        // bigSmall(SUM_OF_THREE, 14, 27, 0, 13),
+        // oddEven(SUM_OF_THREE),
+        ...getWinCombinations(...DRAW_NUMBERS.slice(2))
+      );
+      break;
+    }
+    case "last 3 sums": {
+      GAME_RESULTS.push(sumAndRange(DRAW_NUMBERS.slice(2)));
+      break;
+    }
+    case "ball position": {
+      const ballPositionObject = positionOfBallNode(DRAW_NUMBERS);
+      const ballPositionValues = Object.values(ballPositionObject);
+      const selectedBallPosition = ballPositionValues[optionalBallPosition - 1];
+      const nonNumericValues = Object.values(selectedBallPosition).filter(
+        (item) => typeof item !== "number"
+      );
+      GAME_RESULTS.push(...nonNumericValues);
+      break;
+    }
+  }
+  return GAME_RESULTS;
+}
+
+function pk10FantanResults(drawNumber, game, optionalBallPosition = 1) {
+  const DRAW_NUMBERS = [...drawNumber];
+  const FIRST_DRAW_NUMBER = [...drawNumber][0];
+  const GAME_RESULTS = [];
+  const SUM_OF_THREE = DRAW_NUMBERS.slice(0, 3).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+  switch (game.toLowerCase()) {
+    case "main area": {
+      const ballPositionObject = positionOfBallNode([FIRST_DRAW_NUMBER]);
+      const ballPositionValues = Object.values(ballPositionObject);
+      const selectedBallPosition = ballPositionValues[optionalBallPosition - 1];
+      const nonNumericValues = Object.values(selectedBallPosition).filter(
+        (item) => typeof item !== "number"
+      );
+      GAME_RESULTS.push(
+        ...nonNumericValues,
+        bigSmall(SUM_OF_THREE, 17, 27, 6, 16),
+        oddEven(SUM_OF_THREE)
+      );
+      break;
+    }
+    case "fantan 1": {
+      GAME_RESULTS.push(...fantan1(SUM_OF_THREE));
+      break;
+    }
+    case "fantan 2": {
+      GAME_RESULTS.push(...fantan2(SUM_OF_THREE));
+      break;
+    }
+    case "ball position": {
+      const ballPositionObject = positionOfBallNode(DRAW_NUMBERS);
+      const ballPositionValues = Object.values(ballPositionObject);
+      const selectedBallPosition = ballPositionValues[optionalBallPosition - 1];
+      const nonNumericValues = Object.values(selectedBallPosition).filter(
+        (item) => typeof item !== "number"
+      );
+      GAME_RESULTS.push(...nonNumericValues);
+      break;
+    }
+  }
+  return GAME_RESULTS;
+}
+
+function fast3FantanResults(drawNumber, game, optionalBallPosition = 1) {
+  const DRAW_NUMBERS = [...drawNumber];
+  // const FIRST_DRAW_NUMBER = [...drawNumber][0];
+  const GAME_RESULTS = [];
+  const SUM_OF_THREE = DRAW_NUMBERS.slice(0, 3).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+  switch (game.toLowerCase()) {
+    case "main area": {
+      GAME_RESULTS.push(
+        bigSmall(SUM_OF_THREE, 11, 17, 4, 10),
+        oddEven(SUM_OF_THREE),
+        naturalElements(SUM_OF_THREE),
+        ...DRAW_NUMBERS.map(String)
+      );
+      break;
+    }
+    case "short deck dice": {
+      const [num1, num2, num3] = DRAW_NUMBERS;
+
+      GAME_RESULTS.push(
+        `${SUM_OF_THREE}`,
+        sPair([num1, num2, num3])[0] ? sPair([num1, num2, num3])[1].join(",") : "",
+        hasTriple([num1, num2, num3]) ? DRAW_NUMBERS.map(String).join(",") : ""
+      );
+      break;
+    }
+    case "miscellaneous long deck": {
+      GAME_RESULTS.push(...generateAllPairs(DRAW_NUMBERS).map(String));
+      break;
+    }
+    case "ball position": {
+      const ballPositionObject = positionOfBallNode(DRAW_NUMBERS);
+      const ballPositionValues = Object.values(ballPositionObject);
+      const selectedBallPosition = ballPositionValues[optionalBallPosition - 1];
+      const nonNumericValues = Object.values(selectedBallPosition).filter(
+        (item) => typeof item !== "number"
+      );
+      GAME_RESULTS.push(...nonNumericValues);
+      break;
+    }
+  }
+  return GAME_RESULTS.filter((item) => item !== "");
+}
+
+function returnCountOfNumbers(drawNumbers) {
+  let count = {};
+  drawNumbers.forEach((item) => {
+    if (count[item]) {
+      count[item] += 1;
+    } else {
+      count[item] = 1;
+    }
+  });
+  return count;
+}
+
+function hasTriple(drawNumbers) {
+  const countOfNumbers =
+    Object.values(returnCountOfNumbers(drawNumbers))[0] === 3;
+
+  return countOfNumbers;
+}
+
+function hasPair(drawNumbers) {
+  const countOfNumbers = Object.values(
+    returnCountOfNumbers(drawNumbers)
+  ).includes(2);
+
+  return countOfNumbers;
+}
+
+function sPair(drawNumbers) {
+  const countOfNumbers = returnCountOfNumbers(drawNumbers);
+  
+  const qualifiedAsPair = Object.values(
+    returnCountOfNumbers(drawNumbers)
+  ).includes(2);
+  
+  const pairNumbers = qualifiedAsPair
+    ? drawNumbers.filter((key) => countOfNumbers[key] >= 2)
+    : [];
+  
+    return [pairNumbers.length > 0, pairNumbers.map(String)];
+}
+
+console.log(
+  // fast3FantanResults([1, 3, 6], "main area")
+  // fast3FantanResults([1, 3, 3], "Short deck dice", 1),
+  // getNumbersInDraw([0, 5, 2])
+  // sPair([0, 0, 2])
+  fast3FantanResults([2, 4, 1], "Miscellaneous Long Deck", 2)
+);
+
+
+function generateAllPairs(numbers) {
+  const pairs = [];
+  const uniquePairs =  Array.from(new Set(numbers))
+  for (let i = 0; i < uniquePairs.length; i++) {
+    for (let j = i + 1; j < uniquePairs.length; j++) {
+      pairs.push([uniquePairs[i], uniquePairs[j]]);
+    }
+  }
+
+  return pairs;
+}
+
