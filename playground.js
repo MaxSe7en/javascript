5069039aa89118937575509bab601538c7a6686d
Max <nanayawfixing@gmail.com>
2024-05-17 11:58:52 +0000

    fantan misc

--- playground.js
+++ playground.js
@@ -16,6 +16,14 @@ function oddEven(num) {
   }
 }
 
+let expectedResults = [
+  "TOAK",
+  "Streak",
+  "Pair",
+  "Half Streak",
+  "Lose Cards",
+];
+
 function fantan1(num) {
   let remainder = num % 4;
   if (remainder === 0) {
@@ -378,13 +386,18 @@ function fast3FantanResults(drawNumber, game, optionalBallPosition = 1) {
 
       GAME_RESULTS.push(
         `${SUM_OF_THREE}`,
-        sPair([num1, num2, num3])[0] ? sPair([num1, num2, num3])[1].join(",") : "",
+        sPair([num1, num2, num3])[0]
+          ? sPair([num1, num2, num3])[1].join(",")
+          : "",
         hasTriple([num1, num2, num3]) ? DRAW_NUMBERS.map(String).join(",") : ""
       );
       break;
     }
     case "miscellaneous long deck": {
-      GAME_RESULTS.push(...generateAllPairs(DRAW_NUMBERS).map(String));
+      GAME_RESULTS.push(
+        ...generateAllPairs(DRAW_NUMBERS).map(String),
+        ...miscellaneousGames(DRAW_NUMBERS)
+      );
       break;
     }
     case "ball position": {
@@ -430,16 +443,16 @@ function hasPair(drawNumbers) {
 
 function sPair(drawNumbers) {
   const countOfNumbers = returnCountOfNumbers(drawNumbers);
-  
+
   const qualifiedAsPair = Object.values(
     returnCountOfNumbers(drawNumbers)
   ).includes(2);
-  
+
   const pairNumbers = qualifiedAsPair
     ? drawNumbers.filter((key) => countOfNumbers[key] >= 2)
     : [];
-  
-    return [pairNumbers.length > 0, pairNumbers.map(String)];
+
+  return [pairNumbers.length > 0, pairNumbers.map(String)];
 }
 
 console.log(
@@ -447,13 +460,12 @@ console.log(
   // fast3FantanResults([1, 3, 3], "Short deck dice", 1),
   // getNumbersInDraw([0, 5, 2])
   // sPair([0, 0, 2])
-  fast3FantanResults([2, 4, 1], "Miscellaneous Long Deck", 2)
+  fast3FantanResults([1, 4, 6], "Miscellaneous Long Deck", 2)
 );
-
 
 function generateAllPairs(numbers) {
   const pairs = [];
-  const uniquePairs =  Array.from(new Set(numbers))
+  const uniquePairs = Array.from(new Set(numbers));
   for (let i = 0; i < uniquePairs.length; i++) {
     for (let j = i + 1; j < uniquePairs.length; j++) {
       pairs.push([uniquePairs[i], uniquePairs[j]]);
@@ -463,3 +475,77 @@ function generateAllPairs(numbers) {
   return pairs;
 }
 
+function miscellaneousGames(drawNumbers) {
+  let values = sortNumbersInArray(
+    Object.values(returnCountOfNumbers(drawNumbers)),
+    "asc"
+  );
+  let b = [];
+  let sorted = JSON.stringify(values);
+
+  switch (sorted) {
+    case "[3]":
+      b.push(expectedResults[0]);
+      break;
+    case "[1,1,1]":
+      if (isStreak(drawNumbers, 3)) {
+        b.push(expectedResults[1]);
+      } else if (isStreak(drawNumbers, 2)) {
+        b.push(expectedResults[3]);
+      } else {
+        b.push(expectedResults[4]);
+      }
+      break;
+    case "[1,2]":
+      b.push(expectedResults[2]);
+      break;
+    default:
+      return [];
+  }
+  return b;
+}
+
+function sortNumbersInArray(drawNumbers, order = "asc") {
+  if (order === "desc") {
+    return drawNumbers.sort((a, b) => b - a);
+  } else {
+    return drawNumbers.sort((a, b) => a - b);
+  }
+}
+
+function isStreak(drawNumber, streakLength) {
+  const sortedDrawNumbers = [...drawNumber].sort((a, b) => a - b);
+
+  for (let i = 0; i < sortedDrawNumbers.length; i++) {
+    let count = 1;
+    let currentIndex = i;
+
+    for (let j = 0; j < streakLength - 1; j++) {
+      const nextIndex = (currentIndex + 1) % sortedDrawNumbers.length;
+
+      if (
+        sortedDrawNumbers[nextIndex] === sortedDrawNumbers[currentIndex] + 1 ||
+        (sortedDrawNumbers[nextIndex] === 0 &&
+          sortedDrawNumbers[currentIndex] === 9)
+      ) {
+        count++;
+        currentIndex = nextIndex;
+      } else {
+        break;
+      }
+    }
+
+    if (count === streakLength) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function isHalfStreak(array) {
+  let streak = sortNumbersInArray(array).some((_, i, arr) => {
+    return arr[i] + 1 === arr[i + 1] || arr[i] + 9 === arr[arr.length - 1];
+  });
+  return streak;
+}
